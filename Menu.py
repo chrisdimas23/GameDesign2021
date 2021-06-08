a844746e0d790aa921bfc5bd22a994394e214ff1
chrisdimas23 <85187264+chrisdimas23@users.noreply.github.com>
2021-06-08 11:34:59 -0500

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,10 @@
+
+print("menu")
+print("//Select a Level//")
+print("level 1")
+print("level 2")
+print("print scores")
+print("exit game")
+var=input()
+for var in range(1,2):  
+    print("level" + var)
